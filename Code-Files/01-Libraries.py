f127ef25402529b9e832a03d081f084d42281b01
Pritam <pritamchakrabortyshuvo@gmail.com>
2024-10-08 21:50:25 +0600

    Library Files added

--- Code-Files/01-Libraries.py
+++ Code-Files/01-Libraries.py
@@ -1,42 +1,23 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import numpy as np\n",
-    "import cv2\n",
-    "import os\n",
-    "import pickle\n",
-    "import tensorflow as tf\n",
-    "from tensorflow.keras.models import Model\n",
-    "from tensorflow.keras.layers import Conv3D, MaxPooling3D, Flatten, Dense, LSTM, TimeDistributed, Input\n",
-    "from tensorflow.keras.utils import to_categorical\n",
-    "from tensorflow.keras.optimizers import Adam\n",
-    "from sklearn.model_selection import train_test_split\n",
-    "from tensorflow.keras.preprocessing.image import img_to_array, load_img\n",
-    "from sklearn.preprocessing import LabelEncoder\n",
-    "from tensorflow.keras.utils import to_categorical\n",
-    "# Model Libraries\n",
-    "import tensorflow as tf\n",
-    "from tensorflow.keras.layers import Conv3D, MaxPooling3D, Flatten, TimeDistributed, LSTM, Dense, Dropout\n",
-    "from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping\n",
-    "from tensorflow.keras.models import Sequential\n",
-    "from tensorflow.keras.layers import TimeDistributed, LSTM, Dense, Dropout, Flatten\n",
-    "from tensorflow.keras.optimizers import Adam\n",
-    "from tensorflow.keras.layers import BatchNormalization\n",
-    "from tensorflow.keras.applications import InceptionV3\n",
-    "from tensorflow.keras.regularizers import l2"
-   ]
-  }
- ],
- "metadata": {
-  "language_info": {
-   "name": "python"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+import numpy as np
+import cv2
+import os
+import pickle
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Conv3D, MaxPooling3D, Flatten, Dense, LSTM, TimeDistributed, Input
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.utils import to_categorical
+# Model Libraries
+import tensorflow as tf
+from tensorflow.keras.layers import Conv3D, MaxPooling3D, Flatten, TimeDistributed, LSTM, Dense, Dropout
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import TimeDistributed, LSTM, Dense, Dropout, Flatten
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.applications import InceptionV3
+from tensorflow.keras.regularizers import l2
