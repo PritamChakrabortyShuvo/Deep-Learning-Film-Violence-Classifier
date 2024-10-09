e05cacc936b4c978efb2bfb411b7fa87d6d5da3e
Pritam <pritamchakrabortyshuvo@gmail.com>
2024-10-09 13:33:16 +0600

    Edited

--- Code-Files/05-Model-Training.py
+++ Code-Files/05-Model-Training.py
@@ -3,8 +3,8 @@ model_checkpoint = ModelCheckpoint('/content/drive/MyDrive/inception_lstm_16_50.
 
 history = model.fit(X_train, y_train,
                      validation_data=(X_val, y_val),
-                     epochs=50,
-                     batch_size=16,
+                     epochs= #add your batch and epoch here
+                     batch_size= #add your batch and epoch here
                      callbacks=[early_stopping, model_checkpoint])
 with open('/content/drive/MyDrive/training_history_16_50.pkl', 'wb') as file:
     pickle.dump(history.history, file)
